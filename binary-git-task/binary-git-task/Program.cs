0ff90151bd2f0c47fd3abf01796bbc0aa3c65bba
Turash Jurij <yrik.turash@gmail.com>
2015-07-03 22:12:13 +0300

    11: Implement sine calculation

--- binary-git-task/binary-git-task/Program.cs
+++ binary-git-task/binary-git-task/Program.cs
@@ -10,6 +10,9 @@ namespace binary_git_task
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter number for calculations:");
+            int num = Console.Read();
+            Console.WriteLine("Sine of {0} is {1}", num, Math.Sin(num));
         }
     }
 }
