38e0258b5ccf7537827cee74f7fc2dc6589a72d4
Turash Jurij <yrik.turash@gmail.com>
2015-07-03 22:36:59 +0300

    111: Implement factorial calculation

--- binary-git-task/binary-git-task/Program.cs
+++ binary-git-task/binary-git-task/Program.cs
@@ -10,7 +10,7 @@ namespace binary_git_task
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Type an double:");
+            Console.WriteLine("Type an double for sine:");
             string line = Console.ReadLine();
             double value;
             if (double.TryParse(line, out value))
@@ -22,6 +22,35 @@ namespace binary_git_task
                 Console.WriteLine("Not an double!");
             }
             Console.ReadKey();
+
+            Console.WriteLine("\n\rType an integer for factorial:");
+            line = Console.ReadLine(); // Read string from console
+            int intVal;
+            if (int.TryParse(line, out intVal))
+            {
+                Console.WriteLine("Sine of {0} is {1}", intVal, Factorial(intVal));
+            }
+            else
+            {
+                Console.WriteLine("Not an integer!");
+            }
+            line = Console.ReadLine(); // Read string from console
+        }
+
+        static int Factorial(int x)
+        {
+            if (x < 0)
+            {
+                return -1;
+            }
+            else if (x == 1 || x == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return x * Factorial(x - 1);
+            }
         }
     }
 }
